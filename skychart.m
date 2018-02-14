0d22c09d686e214242a248a498e4525e47af0eda
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2018-02-14 11:03:42 +0100

    update to doc

--- skychart.m
+++ skychart.m
@@ -1,12 +1,43 @@
 classdef skychart < handle
-
   % SKYCHART: a class to plot a sky chart with stars/objects
+  %
+  % This class comptes and plots the sky seen at given location and time. About
+  % 43000 stars and 13000 deep sky objects are considered, as well as the Sun, the 
+  % Moon and 7 planets. The actual number of rendered objects depends on the zomm 
+  % level in the sky chart.
+  %
+  % You may zoom the plot using the Zoom tool (in the Toolbar). You may as well 
+  % use the drag tool to move the visible area. Right-click shows a contextual
+  % menu with the under-lying object properties (coordinates, type, ...).
+  %
+  % To use this code, type
+  %
+  % >> sc = skychart
+  %
+  % displays the view at the current UTC and location.
+  %
+  % Methods:
+  %   skychart:   create the view
+  %   date:       set/get the date (UTC)
+  %   load:       load the catalogs. Done at start.
+  %   getplace:   get the current GPS location from the network.
+  %   plot:       plot/replot the view.
+  %
+  % You may force a re-computation and replot of the sky view with:
+  %
+  % >> compute(sc, 'force')
+  % >> plot(sc, 'force)
   %
   % Credits:
   % E. Ofek     MAAT            GPL3 2004
   %   http://weizmann.ac.il/home/eofek/matlab/ 
   % F. Glineur  parse_json      BSD  2009
   %   https://fr.mathworks.com/matlabcentral/fileexchange/23393--another--json-parser
+  % P. Tenenbaum Local Time to UTC, BSD 2008
+  %   https://fr.mathworks.com/matlabcentral/fileexchange/22295-local-time-to-utc
+  % Stars (~46000) data base  from http://astrosci.scimuze.com/stellar_data.htm
+  % Deep sky objects (~13000) from http://klima-luft.de/steinicke/ngcic/ngcic_e.htm
+
   
   properties
   
